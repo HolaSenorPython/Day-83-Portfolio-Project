81359d5532e2cfcd557aededcf991d57e93b8b6d
Elisha <pythontestingyay@gmail.com>
2025-06-10 00:36:03 -0400

    Updated main.py and removed dockerfile, switching deploy strat

--- main.py
+++ main.py
@@ -80,5 +80,4 @@ def portfolio():
     return render_template('portfolio.html')
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
